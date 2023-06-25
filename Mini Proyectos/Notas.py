856483dca966e03fd9ba9c1ba9ba76befc317091
Diego <diegosanmartin05@gmail.com>
2023-06-25 00:21:27 -0400

    actualizacion de Notas.py

--- Mini Proyectos/Notas.py
+++ Mini Proyectos/Notas.py
@@ -1,12 +1,27 @@
 
+
+import tkinter             #ventana grafica
+
+ventana = tkinter.Tk()
+ventana.geometry("400x400")
+etiqueta = tkinter.Label(ventana, text = "Generador de notas", bg= "#b3ccff")
+etiqueta.pack(fill=tkinter.X)
+ventana.mainloop()
+
+cantidad = int (input("Ingrese cantidad a calcular : "))
 notas = []
-for i in range (1, 13):
+
+#bucle para ingresar notas
+for i in range (1, cantidad + 1):
     nota = float (input("Notas {} : ".format(i)))
     notas.append(nota)
 
+#Resultado del promedio
 promedio = sum(notas) / len(notas)
 print ("El promedio es: ", promedio)
 
+
+#algunos mensajitos
 if  promedio >= 60 :
     print ("Felicidades")
     print ("(⁠つ⁠✧⁠ω⁠✧⁠)⁠つ")
@@ -20,3 +35,5 @@ else :
        
     if promedio < 30:
         print ("malísimo")
+
+
